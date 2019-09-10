a4f6e409d541ef9e4a4e9eee22403fe5635c60e7
unknown <kprinc8789@yahoo.com>
2019-09-10 18:14:08 -0400

    Added javadoc comments to Subject.java

--- src/Subject.java
+++ src/Subject.java
@@ -6,7 +6,18 @@
  * September 10th, 2019
  */
 public interface Subject {
+	/**
+	 * Adds the passed in observer to an ArrayList, 
+	 * which will all be updated when notifyObservers is called.
+	 */
 	public void registerObserver(Observer observer);
+	/**
+	 * Removes the passed in observer from the ArrayList.
+	 */
 	public void removeObserver(Observer observer);
+	/**
+	 * Goes through the ArrayList of Observers and 
+	 * calls the update method for each of them.
+	 */
 	public void notifyObservers();
 }
